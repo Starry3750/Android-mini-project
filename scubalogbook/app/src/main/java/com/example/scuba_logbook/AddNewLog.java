efd67b00db665a020d896b8d68c4e52538939b3d
In Wook Hwang <cafeterrace3750@gmail.com>
2021-02-14 22:16:35 +0900

    Google Map Setting
    현재 내 위치로 이동하는 기능 추가함.

--- scubalogbook/app/src/main/java/com/example/scuba_logbook/AddNewLog.java
+++ scubalogbook/app/src/main/java/com/example/scuba_logbook/AddNewLog.java
@@ -1,8 +1,12 @@
 package com.example.scuba_logbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,23 +30,36 @@ public class AddNewLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_log);
 
+        // Google Map
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 Log.d("Map", "지도 준비 완료.");
                 map = googleMap;
+                if (ActivityCompat.checkSelfPermission(AddNewLog.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(AddNewLog.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                // 구글맵 시작시, 초기위치를 내 위치로
+                map.setMyLocationEnabled(true);
             }
         });
 
-        try{
+        try {
             MapsInitializer.initialize(this);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         // 차수 라디오버튼 그룹 Check Listener
-        RadioGroup diving_round = (RadioGroup)findViewById(R.id.diving_round);
+        RadioGroup diving_round = (RadioGroup) findViewById(R.id.diving_round);
         diving_round.setOnCheckedChangeListener(radioGroupButtonChangeLister);
 
     }
@@ -51,16 +68,16 @@ public class AddNewLog extends AppCompatActivity {
     // 달력 관련 코드
     public void showDatePicker(View view) {
         DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(),"datePicker");
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
-    public void processDatePickerResult(int year, int month, int day){
-        String month_string = Integer.toString(month+1);
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
         String day_string = Integer.toString(day);
         String year_string = Integer.toString(year);
         String dateMessage = (year_string + "년 " + month_string + "월 " + day_string + "일");
 
-        Toast.makeText(this,"날짜 선택 완료",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "날짜 선택 완료", Toast.LENGTH_SHORT).show();
         TextView diving_date_select_result = findViewById(R.id.diving_date_select_result);
         diving_date_select_result.setText(dateMessage);
     }
@@ -68,12 +85,57 @@ public class AddNewLog extends AppCompatActivity {
     RadioGroup.OnCheckedChangeListener radioGroupButtonChangeLister = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
-            RadioGroup radioGroup = (RadioGroup)findViewById(R.id.diving_round);
+            RadioGroup radioGroup = (RadioGroup) findViewById(R.id.diving_round);
             int RadioId = radioGroup.getCheckedRadioButtonId(); // 체크된 라디오버튼의 ID 리턴
-            RadioButton radioButton = (RadioButton)findViewById(RadioId);
-            Toast.makeText(AddNewLog.this,radioButton.getText().toString() + " 다이빙 선택 완료",Toast.LENGTH_SHORT).show();
+            RadioButton radioButton = (RadioButton) findViewById(RadioId);
+            Toast.makeText(AddNewLog.this, radioButton.getText().toString() + " 다이빙 선택 완료", Toast.LENGTH_SHORT).show();
         }
     };
+
+    // Google Map 관련
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String message = "현재 위치 : Lat(" + latitude + "), Long(" + longitude + ")";
+            Toast.makeText(AddNewLog.this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onResume() {
+        super.onResume();
+        if (map != null) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            map.setMyLocationEnabled(true);
+        }
+    }
+
+    public void onPause() {
+        super.onPause();
+        if (map != null) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            map.setMyLocationEnabled(false);
+        }
+    }
 
 
 
