de82facb533f6b9a9dc6325da673f4de8fb62250
In Wook Hwang <cafeterrace3750@gmail.com>
2021-02-14 21:33:21 +0900

    Google Map Apply
    구글맵 적용 완료.
    기본 위치 조정 및 편의 기능 추가 필요.
    원하는 위치 피킹 후, 주소를 저장하는 기능 추가 필요.

--- scubalogbook/app/src/main/java/com/example/scuba_logbook/AddNewLog.java
+++ scubalogbook/app/src/main/java/com/example/scuba_logbook/AddNewLog.java
@@ -4,18 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class AddNewLog extends AppCompatActivity {
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_log);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.d("Map", "지도 준비 완료.");
+                map = googleMap;
+            }
+        });
+
+        try{
+            MapsInitializer.initialize(this);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         // 차수 라디오버튼 그룹 Check Listener
         RadioGroup diving_round = (RadioGroup)findViewById(R.id.diving_round);
