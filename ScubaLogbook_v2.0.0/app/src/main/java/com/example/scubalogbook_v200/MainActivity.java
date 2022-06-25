570a9d1e3e4d6485b2b701ae97f9d7463ef26f96
In Wook Hwang <cafeterrace3750@gmail.com>
2022-06-25 19:37:51 +0900

    Add Circular Reveal Animation
    - Add Circular Reveal Animation when move to EnterPrevDiveNum from MainActivity.
    - Add ripple effect when "DIVE" button is clicked.

--- ScubaLogbook_v2.0.0/app/src/main/java/com/example/scubalogbook_v200/MainActivity.java
+++ ScubaLogbook_v2.0.0/app/src/main/java/com/example/scubalogbook_v200/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.scubalogbook_v200;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +22,50 @@ public class MainActivity extends AppCompatActivity {
         AddNewLogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EnterPrevDiveNum.class);
-                startActivity(intent);
+
+                View revealView = findViewById(R.id.reveal_layout);     // Layout to use FULL SCREEN animation
+                View buttonView = findViewById(R.id.add_new_log_btn);   // View of "DIVE" button
+
+                int add_new_log_btn_locationX = (int)buttonView.getX();     // X coordinate of "DIVE" button
+                int add_new_log_btn_locationY = (int)buttonView.getY();     // Y coordinate of "DIVE" button
+
+                int cx = buttonView.getWidth() / 2;
+                int cy = buttonView.getHeight() / 2;
+
+                int circular_reveal_animation_start_coordinateX = add_new_log_btn_locationX + cx;
+                int circular_reveal_animation_start_coordinateY = add_new_log_btn_locationY + cy;
+
+                int finalRadius = Math.max(revealView.getWidth(), revealView.getHeight());  // Radius which that Circular reveal animation ends.
+
+                Animator animator =
+                        ViewAnimationUtils.createCircularReveal( // set Animator
+                                revealView,
+                                circular_reveal_animation_start_coordinateX,
+                                circular_reveal_animation_start_coordinateY,
+                                0,
+                                finalRadius);
+
+
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());   // Interpolator for giving effect to animation
+                animator.setDuration(500);                                          // Duration of the animation
+
+                revealView.setVisibility(View.VISIBLE);     // make the view visible
+                animator.start();                           // animation starts
+
+                // When Animation ends
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        revealView.setVisibility(View.INVISIBLE);   // make the view invisible
+
+                        Intent intent = new Intent(MainActivity.this, EnterPrevDiveNum.class);
+                        startActivity(intent);      // move to next activity(Enter Previous Dive Number)
+                    }
+                });
+
             }
         });
     }
-
 
 }
